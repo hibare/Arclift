82fd982a0320c64b9ccc32f8b8749ba7048d2cfe
Shubham Hibare <shubham@hibare.in>
2025-10-23 15:24:51 +0530

    fix(deb): Post installation

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-arclift config init \
+mkdir -p /etc/arclift \
+&& touch /etc/arclift/config.yaml \
+&& arclift config init \
 && systemctl daemon-reload \
 && systemctl enable arclift.service \
 && systemctl start arclift.service
